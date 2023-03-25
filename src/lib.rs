536c24ed2a55cb7b9c799a8d318e893f4e165103
Dylan Carrington-Fair <carringtonfair.dylan@gmail.com>
2023-03-25 19:30:41 -0400

    Trying to learn how to read in a response and execute a binary off of that response

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use glob::{glob_with, MatchOptions};
+use std::path::PathBuf;
+use std::process;
+
 // use glob::glob;
 
 // pub fn find_directory(directory_name: &str) {
@@ -12,3 +16,28 @@
 
 //     println!("Looking for {}", directory_name);
 // }
+
+pub fn search_for_executables(search_term: &str, directories: Vec<&str>) -> Vec<PathBuf> {
+    if directories.len() == 0 {
+        println!("No game directories to search, are you sure they've been set?");
+        process::exit(1);
+    }
+    // Now search for this file in the directories
+    let options = MatchOptions {
+        case_sensitive: false,
+        require_literal_leading_dot: false,
+        require_literal_separator: false
+    };
+
+    let mut vec_paths:Vec<PathBuf> = vec![];
+    for directory in directories {
+        let search_string = format!("{directory}/**/*{search_term}*.exe");
+        for entry in glob_with(&search_string, options).unwrap() {
+            if let Ok(path) = entry {
+                vec_paths.push(path);
+            }
+        }    
+    }
+
+    vec_paths
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 // use std::env;
-use glob::{glob_with, MatchOptions};
-use std::env;
-
+use std::{env, process, io};
 
 fn main() {
     // Read in search term
@@ -28,22 +26,50 @@ fn main() {
     game_directories.push(steam_path);
     game_directories.push(blizzard_path);
 
-    // Now search for this file in the directories
-    let options = MatchOptions {
-        case_sensitive: false,
-        require_literal_leading_dot: false,
-        require_literal_separator: false
-    };
-
-    for directory in game_directories {
-        let search_string = format!("{directory}/**/*{search_string}*.exe");
-        for entry in glob_with(&search_string, options).unwrap() {
-            if let Ok(path) = entry {
-                println!("{:?}", path.display())
-            }
-        }    
+    // Get executable paths
+    let vec_paths = play::search_for_executables(&search_string, game_directories);
+    println!("{:?}", vec_paths);
+    // Choice logic
+    // No executable found
+    if vec_paths.len() < 1 {
+        println!("Could not find a matching executable.");
+        process::exit(1);
     }
-    
+    // One executable found
+    if vec_paths.len() == 1 {
+        println!("Start the following?:");
+        println!("{:?}", vec_paths[0]);
+        println!("(y/n)");
+        let mut response = String::new();
+        io::stdin().read_line(&mut response).expect("Failed to read response.");
+        let response: &str = match response.trim().expect("Failed to read response.") {
+            Ok(y_or_no) => y_or_no,
+            Err(_) => {
+                println!("Could not read input.");
+                "Fail"
+            }, 
+        };
 
+        match response {
+            "y" => {
+                process::Command::new(vec_paths[0]).output().expect("failed to execute process");
+                println!("Executed process!");
+                process::exit(1);
+            },
+            "n" => process::exit(1),
+            _ => process::exit(1),
+        }
+    }
+    // More than one executable found
+    if vec_paths.len() > 1 {
+        let vec_length = vec_paths.len();
+        let mut count = 1;
+        println!("Multiple executables found, which one would you like to start?");
+        for path in vec_paths {
+            println!("{}. {:?}", count, path);
+            count += 1;
+        }
+        println!("{}. Cancel", vec_length + 1);
+    }
     
 }
