b10219a717ccff7a2da896b045a65d98e5b16448
Dylan Carrington-Fair <carringtonfair.dylan@gmail.com>
2023-03-26 12:41:25 -0400

    Program now can handle the multi-choice cases. Need to test with actual libraries set

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,62 @@
 use glob::{glob_with, MatchOptions};
 use std::path::PathBuf;
-use std::process;
+use std::str::FromStr;
+use std::{process, io, env};
 
-// use glob::glob;
+pub struct Config {
+    pub search_string: String,
+    pub game_directories: Vec<String>,
+}
 
-// pub fn find_directory(directory_name: &str) {
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("Need to supply a search term");
+        }
+        if args.len() > 2 {
+            return Err("Too many arguments supplied.");
+        }
+    
+        let search_string = args[1].clone();
 
-//     let mut entry_vecs:Vec<&str> = vec![];
-//     for entry in glob("**/*.exe").expect("Errored at first glob?") {
-//         let entry_PathBuf = entry.expect("Couldn't find entry");
-//         let entry_string = entry_PathBuf.to_str().expect("Couldn't get a string");
-//         entry_vecs.push(entry_string);
-//         println!("{}", entry.expect("Errored at entry?").display());
-//     }
 
-//     println!("Looking for {}", directory_name);
-// }
+        // let mut game_directories_env: Vec<String> = vec![];
 
-pub fn search_for_executables(search_term: &str, directories: Vec<&str>) -> Vec<PathBuf> {
+        // let env_steam_path = env::var("steam_path").unwrap();
+        // let env_blizzard_path = env::var("blizzard_path").unwrap();
+    
+        // game_directories_env.push(env_steam_path);
+        // game_directories_env.push(env_blizzard_path);
+    
+        let steam_path = String::from_str("/home/dylan/Documents/play-test/SteamLibrary").unwrap();
+        let blizzard_path = String::from_str("/home/dylan/Documents/play-test/BlizzardLibrary").unwrap();
+        // PC paths
+        let _steam_path_pc = "";
+        let _blizzard_path_pc = "";
+    
+        // Put directories in a vector
+        let mut game_directories: Vec<String> = vec![];
+        game_directories.push(steam_path);
+        game_directories.push(blizzard_path);
+
+
+        Ok(Config { search_string, game_directories, })
+    }
+}
+
+pub fn search_for_executables(search_term: &str, directories: Vec<String>) -> Vec<PathBuf> {
     if directories.len() == 0 {
         println!("No game directories to search, are you sure they've been set?");
         process::exit(1);
     }
-    // Now search for this file in the directories
+   
+    // Options we can set for our search
     let options = MatchOptions {
         case_sensitive: false,
         require_literal_leading_dot: false,
         require_literal_separator: false
     };
-
+    // Now search for this file in the directories
     let mut vec_paths:Vec<PathBuf> = vec![];
     for directory in directories {
         let search_string = format!("{directory}/**/*{search_term}*.exe");
@@ -40,4 +68,86 @@ pub fn search_for_executables(search_term: &str, directories: Vec<&str>) -> Vec<
     }
 
     vec_paths
+}
+
+pub fn run_paths(paths: Vec<PathBuf>) {
+    // Choice logic
+    // No executable found
+    if paths.len() < 1 {
+        println!("Could not find a matching executable.");
+        process::exit(1);
+    }
+
+
+    let mut response = String::new();
+    // One executable found
+    if paths.len() == 1 {
+        // Print opening choice
+        println!("Start the following?:");
+        println!("{:?}", paths[0]);
+        println!("(y/n)");
+        // Read in user response
+        io::stdin().read_line(&mut response).expect("Failed to read response.");
+        // match response to an outcome
+        match response.trim() {
+            "y" => {
+                let exe_path = paths[0].to_str().expect("Failed to stringify path");
+                process::Command::new(exe_path).output().expect("failed to execute process");
+                println!("Executed process!");
+                process::exit(1);
+            },
+            "n" => process::exit(1),
+            _ => {
+                println!("Please respond with a 'y' or 'n'");
+                process::exit(1);
+            },
+        };
+    }
+    // More than one executable found
+    if paths.len() > 1 {
+        // Print opening choices
+        let vec_length = paths.len();
+        let mut count = 1;
+        println!("Multiple executables found, which one would you like to start?");
+        for path in &paths {
+            println!("{}. {:?}", count, path);
+            count += 1;
+        }
+        println!("{}. Cancel", vec_length + 1);
+        // Read in user response
+        io::stdin().read_line(&mut response).expect("Failed to read response.");
+        let choice: usize = match response.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Could not read your input, please try a valid number next time");
+                process::exit(1);
+            },
+        };
+
+
+        // Match response to an outcome
+        match choice > 0 {
+            true => {
+                if choice > 0 && choice < vec_length + 1 {
+                    let path_slice = choice - 1;
+                    let exe_path = paths[path_slice].to_str().expect("Failed to read number.");
+                    process::Command::new(exe_path).output().expect("failed to execute process");
+                    println!("Executed process!");
+                    process::exit(1);
+                }
+                if choice == vec_length + 1 {
+                    process::exit(1)
+                }
+                if choice > vec_length + 1 {
+                    println!("Please choose a number within the correct range.");
+                    process::exit(1)
+                }
+            }
+            _ => {
+                println!("Please respond with a valid number.");
+                process::exit(1);
+            }
+        }
+
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,20 @@
 // use std::env;
-use std::{env, process, io};
+use std::{env, process};
 
 fn main() {
     // Read in search term
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 2 {
-        return println!("Need to supply a search term");
-    }
-    if args.len() > 2 {
-        return println!("Too many arguments supplied.");
-    }
+    let config = play::Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let search_string = args[1].clone();
-    
-    // Linux paths
-    let steam_path = "/home/dylan/Documents/play-test/SteamLibrary";
-    let blizzard_path = "/home/dylan/Documents/play-test/BlizzardLibrary";
-    // PC paths
-    let _steam_path_pc = "";
-    let _blizzard_path_pc = "";
-
-    // Put directories in a vector
-    let mut game_directories: Vec<&str> = vec![];
-    game_directories.push(steam_path);
-    game_directories.push(blizzard_path);
+    println!("{:?}", config.game_directories);
 
     // Get executable paths
-    let vec_paths = play::search_for_executables(&search_string, game_directories);
+    let vec_paths = play::search_for_executables(&config.search_string, config.game_directories);
     println!("{:?}", vec_paths);
-    // Choice logic
-    // No executable found
-    if vec_paths.len() < 1 {
-        println!("Could not find a matching executable.");
-        process::exit(1);
-    }
-    // One executable found
-    if vec_paths.len() == 1 {
-        println!("Start the following?:");
-        println!("{:?}", vec_paths[0]);
-        println!("(y/n)");
-        let mut response = String::new();
-        io::stdin().read_line(&mut response).expect("Failed to read response.");
-        let response: &str = match response.trim().expect("Failed to read response.") {
-            Ok(y_or_no) => y_or_no,
-            Err(_) => {
-                println!("Could not read input.");
-                "Fail"
-            }, 
-        };
 
-        match response {
-            "y" => {
-                process::Command::new(vec_paths[0]).output().expect("failed to execute process");
-                println!("Executed process!");
-                process::exit(1);
-            },
-            "n" => process::exit(1),
-            _ => process::exit(1),
-        }
-    }
-    // More than one executable found
-    if vec_paths.len() > 1 {
-        let vec_length = vec_paths.len();
-        let mut count = 1;
-        println!("Multiple executables found, which one would you like to start?");
-        for path in vec_paths {
-            println!("{}. {:?}", count, path);
-            count += 1;
-        }
-        println!("{}. Cancel", vec_length + 1);
-    }
-    
+    play::run_paths(vec_paths);    
 }
